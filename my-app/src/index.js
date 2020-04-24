521406a388a1e06728867d8efb40b79f0cb27fe5
paggala <paggala@ges.com>
2020-04-24 13:05:05 +0530

    Modified Index

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+//Added comment to modify
+
 ReactDOM.hydrate(
   <App />,
   document.getElementById('mountNode'),
