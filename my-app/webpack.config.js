133b2be7ffa3e48eb2762912ef87fdd98742c2e5
134265 <134265@HCHLH0718LP4508>
2020-04-27 08:56:17 +0530

    Space Inclusion Commit

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
-          },
-        },
+          }, 
+        }, 
       ],
     },
   };
